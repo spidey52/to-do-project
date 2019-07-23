f196d8a556b25a61ff90c54f76acfb47d0f681e7
satyam kumar <satyamkumar5254@gmail.com>
2019-07-24 01:14:01 +0530

    deleting the todos functionality works

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,22 @@
-$("li").click(function(){
-    if($(this).css("color") === "rgb(128, 128, 128)"){
-       $(this).css("color", "black")
-       $(this).css("text-decoration", "none")
-    }else{
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        })
-    }
-    
+// $("li").click(function(){
+//     if($(this).css("color") === "rgb(128, 128, 128)"){
+//        $(this).css("color", "black")
+//        $(this).css("text-decoration", "none")
+//     }else{
+//         $(this).css({
+//             color: "gray",
+//             textDecoration: "line-through"
+//         })
+//     }
+// })
+
+$('li').click(function(){
+    $(this).toggleClass('completed')
+})
+
+$('span').click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove()
+    })
+    event.stopPropagation();
 })
